52a2defe376df1124f00075d8989b05e7f492c28
Elise CUNNIET--ROBERT <ecunniet@e1r7p6.42.fr>
2019-11-02 18:40:34 +0100

    did the regex, it was much better - next step, resolving equations

--- computerv1.py
+++ computerv1.py
@@ -1,37 +1,54 @@
 import sys
-
-def all_ex(split, sign):
-    nbrs = []
-    start = 0
-    print (sign)
-    space_index = split.find(sign)
-    while space_index != -1:
-        nbrs.append(split[start:space_index])
-        start = (space_index + 1)
-        space_index = split.find(sign, start)
-    nbrs.append(split[start:])
-    return (nbrs)
+import re
 
 def split_eq(eq):
-    nbrs = []
-    # separe les deux cotes
-    j = 0
-    split_half = eq.split('=')
-    for split in split_half:
-        nbrs.append(all_ex(split, "+"))
-        for nb in nbrs[len(nbrs) - 1]:
-            tmp = all_ex(nb, '-')
-            if len(tmp) > 1:
-                i = 0
-                while i < len(tmp):
-                nbrs[len(nbrs) - 1] = tmp[i][len(nbrs) - 1]all_ex(nb, '-')
-                i++
-    return ('hello')
+    eq = re.sub("\s", '', eq)
+    x = re.split("=", eq, 1)
+    print x
+    side_1 = re.findall("(?:(?:-|[+])\s*)?(?:\d*\.)?\d+\s*\*\s*[xX]\^\d+",x[0])
+    side_2 = re.findall("(?:(?:-|[+])\s*)?(?:\d*\.)?\d+\s*\*\s*[xX]\^\d+", x[1])
+    for all in side_2:
+        if all[0] == '-':
+            side_1.append('+' + all[1:])
+        elif all[0] == '+':
+            side_1.append('-' + all[1:])
+        else:
+            side_1.append('-' + all)
+    for expression in side_1:
+        print expression,
+    print '= 0'
+    return (side_1)
 
+def reduce_eq(eq):
+    new = []
+    i = 0
+    max = len(eq)
+    while i < max:
+        match = re.findall("[xX]\^(\d+)", eq[i])[0]
+        j = i + 1
+        count = float(re.findall("((?:(?:-|[+])\s*)?(?:\d*\.)?\d+)\s*\*\s*[xX]\^\d+", eq[i])[0])
+        print count
+        while j < max:
+            if(match == re.findall("[xX]\^(\d+)", eq[j])[0]):
+                count = count + float(re.findall("((?:(?:-|[+])\s*)?(?:\d*\.)?\d+)\s*\*\s*[xX]\^\d+", eq[j])[0])
+                print j,
+                print " = ",
+                print count
+                print eq
+                del eq[j]
+                print eq
+                max -= 1
+                print max
+            else:
+                j += 1
+        new.append(count)
+        i += 1
+    print new
+    return 0
 # quand on lance le programme
 if __name__ == "__main__":
     # verifier pas trop arguments? vous si 3 calculs possible en meme temps en bonus
     i = len(sys.argv)
     if i == 2:
         eq = split_eq(sys.argv[1])
-        print(eq)
+        reduce_eq(eq)
