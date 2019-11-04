01f37c7fa2a4b4d76bebbe04758d1ef774ae75c2
Elise CUNNIET--ROBERT <ecunniet@e1r7p6.42.fr>
2019-11-04 01:01:18 +0100

    I had a hard time with the sqrt function but i finaly understood the logic, this program is missing a checking for the 0 = 0 case and the first degree case too but it'll be fine

--- computerv1.py
+++ computerv1.py
@@ -69,6 +69,47 @@ def check_degre(eq):
     if count > 2:
         print "The polynomial degree is stricly greater than 2, I can't solve."
         sys.exit(0)
+    return count
+
+def find_coef(tab_1,tab_2):
+    count = 0
+    coef = [None, None, None]
+    max = len(tab_2)
+    while count < 3:
+        i = 0
+        while i < max:
+            tmp = int(re.findall("[xX]\^(\d+)", tab_2[i])[0])
+            if tmp == count:
+                coef[count] = tab_1[i]
+            i += 1
+        if coef[count] is None:
+            coef[count] = 0
+        count +=1
+    print coef,
+    return coef
+
+def racine_carre(delta):
+    x1 = (delta * 1.0) / 2 #le nombre max de la racine  1
+    x2 = (x1 + (delta / x1)) / 2
+    while (abs(x1 - x2) > 0):
+        x1 = x2                        #on le passe parce que ce seront les meme resultats de toute facon
+        x2 = (x1 + (delta / x1)) / 2 # la racine est trouve quand 9 / 3 = 3 et 3 + 3 /2  = 3
+    return x2
+
+def solve_the_equation(tab_1, tab_2):
+    coef = find_coef(tab_1,tab_2)
+    delta = coef[1] * coef[1] - 4 * coef[2] * coef[0]
+    if delta > 0:
+        solution_1 = (- coef[1] - racine_carre(delta)) / (2 * coef[2])
+        solution_2 = (- coef[1] + racine_carre(delta)) / (2 * coef[2])
+        print("%.6f" % solution_1)
+        print("%.6f" % solution_2)
+    elif delta == 0:
+        solution = (- coef[1]) / (2 * coef[2])
+        print solution
+    else:
+        print "pas de solution"
+    return 0
 
 # quand on lance le programme
 if __name__ == "__main__":
@@ -77,4 +118,8 @@ if __name__ == "__main__":
     if i == 2:
         eq = split_eq(sys.argv[1])
         double_tab = reduce_eq(eq)
-        check_degre(double_tab[1])
+        degree = check_degre(double_tab[1])
+        if degree == 2:
+            solve_the_equation(double_tab[0], double_tab[1])
+        else:
+            print "a faire plus tard"
