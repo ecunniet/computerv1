b1d601135c75fc0185122a0f21bf3112c2ce443c
Elise CUNNIET--ROBERT <ecunniet@e1r9p9.42.fr>
2019-11-05 15:44:19 +0100

    Done with the complex number for degree 2 - next step - degree 1

--- computerv1.py
+++ computerv1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# coding: utf-8
 import sys
 import re
 
@@ -71,13 +73,17 @@ def check_degre(eq):
         sys.exit(0)
     return count
 
-def is_soluble(new):    #check if equation is soluble
-    ok = 0
-    for all in new:
-        if all != 0:
-            ok = 1
-    if ok == 0 :
-        print "X could be any real number."
+def is_soluble(double_tab):    #check if equation is soluble
+    expo_0 = 0
+    coef_0 = 0
+    for eq in double_tab[1]:
+        if int(re.findall("[xX]\^(\d+)", eq)[0]) != 0:
+            expo_0 = 1
+    for new in double_tab[0]:
+        if double_tab[0] != 0:
+            coef_0 = 1
+    if expo_0 == 0 or coef_0 == 0 :
+        print "The solution could be any real number."
         sys.exit(0)
 
 def find_coef(tab_1,tab_2):
@@ -97,10 +103,17 @@ def find_coef(tab_1,tab_2):
     print coef,
     return coef
 
+def abs_1(nb):
+    if nb < 0:
+        nb *= -1
+    if nb != 0:
+        nb = (nb * nb) / nb
+    return nb
+
 def racine_carre(delta): #j aurai pu faire delta**05 mais je suis pas sur que ce soit autorise
     x1 = (delta * 1.0) / 2 #le nombre max de la racine  1
     x2 = (x1 + (delta / x1)) / 2
-    while (abs(x1 - x2) > 0):
+    while (abs_1(x1 - x2) > 0):
         x1 = x2                        #on le passe parce que ce seront les meme resultats de toute facon
         x2 = (x1 + (delta / x1)) / 2 # la racine est trouve quand 9 / 3 = 3 et 3 + 3 /2  = 3
     return x2
@@ -109,15 +122,25 @@ def solve_the_equation(tab_1, tab_2):
     coef = find_coef(tab_1,tab_2)
     delta = coef[1] * coef[1] - 4 * coef[2] * coef[0]
     if delta > 0:
+        print ("Discriminant is strictly positive, the two solutions are:")
         solution_1 = (- coef[1] - racine_carre(delta)) / (2 * coef[2])
         solution_2 = (- coef[1] + racine_carre(delta)) / (2 * coef[2])
         print("%.6f" % solution_1)
         print("%.6f" % solution_2)
     elif delta == 0:
+        print("The solution is:")
         solution = (- coef[1]) / (2 * coef[2])
-        print solution
+        print("%.6f" % solution)
     else:
-        print "pas de solution"
+        print ("Discriminant is strictly negative, this equations has no solutions in real numbers but two solutions in complex numbers that are:")
+        print "(",
+        if coef[1] != 0:
+            print (- coef[1]),
+        print "− i√(", -delta, ")) / ", (2 * coef[2]), ")"
+        print "(",
+        if coef[1] != 0:
+            print (- coef[1]), "+",
+        print "i√(", -delta, ")) / ", (2 * coef[2]), ")"
     return 0
 
 # quand on lance le programme
@@ -128,8 +151,9 @@ if __name__ == "__main__":
         eq = split_eq(sys.argv[1])
         double_tab = reduce_eq(eq)
         degree = check_degre(double_tab[1])
-        is_soluble(double_tab[0])
+        is_soluble(double_tab)
         if degree == 2:
             solve_the_equation(double_tab[0], double_tab[1])
         else:
+
             print "a faire plus tard"
